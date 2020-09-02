df19ec1338732cac867a5b3734691bf73301a005
s1mpleTEK <57884979+s1mpleTEK@users.noreply.github.com>
2020-09-02 04:32:55 +0200

    Added raw win condition

--- src/lib/verification_function.py
+++ src/lib/verification_function.py
@@ -6,3 +6,21 @@ class Verification:
                 return True
         print("This raw is full")
         return (False)
+    
+    @classmethod
+    def verification_game_end(cls, table, user):
+        count = 0
+        if user == 1:
+            elem = "O"
+        elif user == 2:
+            elem = "X"
+        for j in range (7):
+            for i in range(5, -1, -1):
+                if table[i][j] == elem:
+                    count += 1
+                    if count == 4:
+                        return(1)
+                else:
+                    count = 0
+
+        return(0)
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from lib.table_function import Table as T
 from lib.user_function import User as U
+from lib.verification_function import Verification as V
 
 def main():
     try:
@@ -9,8 +10,15 @@ def main():
         while 1:
             print(user[user[2]-1],"'s turn")
             table = U().user_interaction(table, user[2])
-            user[2] = U().user_change(user[2])
+            game_end = V.verification_game_end(table, user[2])
             T.display_table(table)
+            if game_end:
+                print(user[user[2]-1]," win the game.")
+                return(0)
+            elif game_end == 2:
+                print("Tie.")
+                return(0)
+            user[2] = U().user_change(user[2])
     except (EOFError, InterruptedError, KeyboardInterrupt):
         exit()
 
